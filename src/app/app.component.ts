e3bb20a872e80fe1c4ff692ec0e1fc55acdc28d0
Julio War <j.guerra@unadeca.net>
2017-04-20 21:24:24 -0600

    adding jquery y assets for bootstrap 3, demo for validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    public offText = "Off Text";
-    public size = "normal";
-    public animate = false;
-    public animated = false;
-    public disabled = false;
-    public readonly = false;
-    public indeterminate = false;
-    public inverse:boolean = true;
-    public color:string = "default";
-    public onText:string = "";
-    public offColor:string = ""
-    public labelText: string = "";
-    public state:boolean = true;
-    public value:boolean = true;
-    public inverseValue:boolean = false;
-    public handleWidth:string = "auto";
-    public labelWidth:string = "auto";
-    public offColorValue:boolean = false;
-    public offTextValue:boolean = false;
+  public offText = 'Off Text';
+  public size = 'normal';
+  public animate = false;
+  public animated = false;
+  public disabled = false;
+  public readonly = false;
+  public indeterminate = false;
+  public inverse: boolean = true;
+  public color: string = 'default';
+  public onText: string = '';
+  public offColor: string = ''
+  public labelText: string = '';
+  public state: boolean = true;
+  public value: boolean = true;
+  public inverseValue: boolean = false;
+  public handleWidth: string = 'auto';
+  public labelWidth: string = 'auto';
+  public offColorValue: boolean = false;
+  public offTextValue: boolean = false;
+  public form: FormGroup;
+  public model: any = {};
 
-    getAlert(value: any):void {
-        alert(value);
-    }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      view: [null, Validators.required]
+    });
+  }
 
-    onChange(event) {
-        console.log(event);
-    }
+  getAlert(value: any): void {
+    alert(value);
+  }
+
+  onChange(event) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { JWBootstrapSwitchModule } from 'jw-bootstrap-switch-ng2';
 
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpModule,
     JWBootstrapSwitchModule
